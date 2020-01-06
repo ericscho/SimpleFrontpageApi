8f3af53dc4fb393708d1854ee4525123f99bdc3d
ericscho <schoneveld@gmail.com>
2020-01-06 17:27:39 +0100

    Update db,js passw
    PW removed

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
-    password : 'Venus2020',
+    password : '-',
     database : 'frontpage'
 });
 
