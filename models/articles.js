92c8b5d5face4969df82dfc5ba28f83341dd17d2
ericscho <schoneveld@gmail.com>
2020-01-06 16:21:02 +0100

    Start Simple API

---
+++ models/articles.js
@@ -0,0 +1,23 @@
+var sql = require('../db.js');
+// dit los trekken
+// voorbeeld https://bezkoder.com/node-js-rest-api-express-mysql/
+
+exports.get_article = (id, result) => {
+    console.log('Artikcle requested wih id ' + id);
+    // sql query database for article with id = :id
+    sql.query("SELECT * FROM frontpage WHERE id = ?",[id],function(err, res) {
+        if(err) {
+            console.log("error: ", err);
+            result(err, null);
+        }
+        else {
+          if (res.length > 0) {
+            console.log("Article found!");
+              result(null, res[0]);
+          }
+          else {
+              console.log("Article not found !");
+          }
+        }
+    });
+}
---
+++ routes/routes.js
@@ -0,0 +1,53 @@
+var articles = require('../models/articles.js');
+
+var appRouter = function(app) {
+  app.get("/", function(req, res) {
+      res.send("Hello World");
+  });
+
+  app.get("/articles/:id", function(req, res) {
+    /*
+      var accountMock = {
+          "username": "frontpage",
+          "password": "Front!page"
+      }
+
+      if(!req.query.username) {
+          return res.send({"status": "error", "message": "missing username"});
+      } else if(req.query.username != accountMock.username) {
+          return res.send({"status": "error", "message": "wrong username"});
+      } else if(req.query.password != accountMock.password) {
+          return res.send({"status": "error", "message": "wrong password"});
+      }
+      */
+
+    articles.get_article(req.params.id, (err,data) => {
+      console.log("ROUTE: Article ID " + req.params.id + " requested");
+        if (err) {
+          if (err.kind === "not_found") {
+            res.status(404).send({
+              message: "Article not found with id ${id}"
+            });
+          }
+          res.status(500).send({message: "An error occured while retrieving article"});
+        }
+        else {
+          res.send(data);
+        }
+
+      });
+
+  });
+
+  /*
+  app.post("/account", function(req, res) {
+      if(!req.body.username || !req.body.password || !req.body.twitter) {
+          return res.send({"status": "error", "message": "missing a parameter"});
+      } else {
+          return res.send(req.body);
+      }
+  });
+  */
+};
+
+module.exports = appRouter;
